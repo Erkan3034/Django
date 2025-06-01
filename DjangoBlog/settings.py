42e276c689f3b5cbe0ced4491156af67008d40ca
Erkan3034 <turguterkan55@gmail.com>
2025-06-01 16:54:18 +0300

    Update settings.py for static file handling: Add WhiteNoise middleware for improved static file management and adjust STATIC_URL to include a leading slash. Refresh requirements.txt and compiled Python bytecode to reflect these changes.

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -133,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR / "static"),
