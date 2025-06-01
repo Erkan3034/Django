294962abc875a497ee548889bac0a39b844c9b9a
Erkan3034 <turguterkan55@gmail.com>
2025-06-01 23:49:24 +0300

    Enable DEBUG mode in settings.py for local development and testing. Update ALLOWED_HOSTS to allow access from localhost and 127.0.0.1.

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['codenthia-com.onrender.com', 'localhost', '127.0.0.1'] # Bu kod, tüm hostların erişimine izin verir(debug modu true olduğu için)
 #ALLOWED_HOSTS = ['localhost', '127.0.0.1']
